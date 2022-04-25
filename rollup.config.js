3936b35f9e5053de0ea7ff5dd5ba0bbfc395cd76
r.ehdals <57896501+kangdongmandoo@users.noreply.github.com>
2022-04-25 20:19:37 +0800

    build: add rollup plugin commonjs

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,19 @@
+import { join } from "path";
 import typescript from "rollup-plugin-typescript2";
+import commonjs from "@rollup/plugin-commonjs";
 
 const base = {
   plugins: [
     typescript({
       tsconfig: "tsconfig.json",
     }),
+    commonjs({ extensions: [".js", ".ts"] }),
   ],
+  external: ["@otox/cli-helper", "@otox/core", "@otox/saver"],
 };
 
 const cfg = (input, output) => {
+  input = join(__dirname, input);
   return {
     ...base,
     input,
@@ -27,7 +32,9 @@ const cfg = (input, output) => {
 
 const configs = [];
 
-configs.push(cfg("./packages/otox/src/index.ts", "./packages/otox/dist/index"));
+configs.push(
+  cfg("./packages/otox-core/src/index.ts", "./packages/otox-core/dist/index")
+);
 
 console.log(JSON.stringify(configs));
 
